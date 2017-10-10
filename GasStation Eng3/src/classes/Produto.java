51c00e068c8ddbdd06a7b4bc41fd1cd0183af33a
j155993 <j155993@LI009>
2017-10-10 11:16:45 -0300

    implementação dos construtores dos produto

--- GasStation Eng3/src/classes/Produto.java
+++ GasStation Eng3/src/classes/Produto.java
@@ -2,6 +2,9 @@ package classes;
 
 public class Produto {
 
+    /**
+     * @return the nome
+     */
     private String nome;
     private int codigo;
     private float valor;
@@ -9,6 +12,38 @@ public class Produto {
     private float dataCompra;
     private String fornecedor;
     private float qntArmazenada;
+    
+    
+    
+    public Produto(){
+    
+    this.codigo=0;
+    this.dataCompra=0;
+    this.descricao=null;
+    this.fornecedor=null;
+    this.nome=null;
+    this.qntArmazenada=0;
+    this.valor=0;  
+    }
+    
+    public Produto(  String nome,
+            int codigo,
+            float valor,
+            String descricao,
+            float dataCompra,
+            String fornecedor,
+            float qntArmazenada){
+        
+        
+        setNome(nome);
+        setCodigo(codigo);
+        setValor(valor);
+        setDescricao(descricao);
+        setDataCompra(dataCompra);
+        setFornecedor(fornecedor);
+        setQntArmazenada(qntArmazenada);
+    
+    }
 
     public void cadastrarProduto() {
     }
@@ -18,4 +53,100 @@ public class Produto {
 
     public void removerProduto() {
     }
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the codigo
+     */
+    public int getCodigo() {
+        return codigo;
+    }
+
+    /**
+     * @param codigo the codigo to set
+     */
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    /**
+     * @return the valor
+     */
+    public float getValor() {
+        return valor;
+    }
+
+    /**
+     * @param valor the valor to set
+     */
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+    /**
+     * @return the descricao
+     */
+    public String getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * @param descricao the descricao to set
+     */
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    /**
+     * @return the dataCompra
+     */
+    public float getDataCompra() {
+        return dataCompra;
+    }
+
+    /**
+     * @param dataCompra the dataCompra to set
+     */
+    public void setDataCompra(float dataCompra) {
+        this.dataCompra = dataCompra;
+    }
+
+    /**
+     * @return the fornecedor
+     */
+    public String getFornecedor() {
+        return fornecedor;
+    }
+
+    /**
+     * @param fornecedor the fornecedor to set
+     */
+    public void setFornecedor(String fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+
+    /**
+     * @return the qntArmazenada
+     */
+    public float getQntArmazenada() {
+        return qntArmazenada;
+    }
+
+    /**
+     * @param qntArmazenada the qntArmazenada to set
+     */
+    public void setQntArmazenada(float qntArmazenada) {
+        this.qntArmazenada = qntArmazenada;
+    }
+
+
 }
